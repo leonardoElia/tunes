b76eb052df0a607ff5a04c4f5323a940a954be01
Leonardo Elias Souza de Oliverira Meireles <leonardoelias802@gmail.com>
2022-10-03 18:03:18 -0300

    fazendo requesito 3 sem arrumar o lint

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { getUser } from '../services/userAPI';
+
+class Header extends React.Component {
+    constructor () {
+        super()
+
+        this.state = {
+            infoUser: 'Carregando...'
+        }
+    }
+
+    nomeUsuario = async () => {
+     const infoUsuario = await getUser();
+     this.setState({
+        infoUser: infoUsuario.name,
+     })
+    }
+    render() {
+        const {infoUser} = this.state;
+        this.nomeUsuario()
+        return(
+       <header data-testid="header-component">
+         <p data-testid="header-user-name">{infoUser}</p>
+       </header>
+        );
+    }
+}
+
+export default Header;
