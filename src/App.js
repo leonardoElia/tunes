8c77daffebb4f05d3e2f9abd58e54ee2adfa4616
Leonardo Elias Souza de Oliverira Meireles <leonardoelias802@gmail.com>
2022-10-03 15:29:39 -0300

    fazendo requesito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/favorites';
+import Profile from './pages/profile';
+import ProfileEdit from './pages/profileEdit';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunesç</p>);
+    return (
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/search" component={ Search } />
+        <Route
+          exact
+          path="/album/:id"
+          render={ (routeProps) => <Album { ...routeProps } /> }
+        />
+        <Route exact path="/favorites" component={ Favorites } />
+        <Route exact path="/profile" component={ Profile } />
+        <Route exact path="/profile/edit" component={ ProfileEdit } />
+        <Route exact path="*" component={ NotFound } />
+      </Switch>
+    );
   }
 }
 
