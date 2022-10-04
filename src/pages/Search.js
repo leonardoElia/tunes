fdf9f23e391e0b3b9805a7b1a0ab07262efb12a6
Leonardo Elias Souza de Oliverira Meireles <leonardoelias802@gmail.com>
2022-10-04 15:03:27 -0300

    fazendo parte do exercicio 6

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,5 +1,8 @@
+import { element } from 'prop-types';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends React.Component {
   constructor() {
@@ -7,6 +10,7 @@ class Search extends React.Component {
     this.state = {
       artista: '',
       ButtonHab: false,
+      artistaInfo: [],
     };
   }
 
@@ -30,8 +34,17 @@ class Search extends React.Component {
     }, this.validacaoButton);
   };
 
+  pesquisar = async () => {
+    const { artista } = this.state;
+    const artistaInfo = await searchAlbumsAPI(artista);
+    this.setState({
+      artista: '',
+      artistaInfo,
+    });
+  };
+
   render() {
-    const { artista, ButtonHab } = this.state;
+    const { artista, ButtonHab, artistaInfo } = this.state;
 
     return (
       <div data-testid="page-search">
@@ -52,10 +65,26 @@ class Search extends React.Component {
           type="button"
           data-testid="search-artist-button"
           disabled={ !ButtonHab }
+          onClick={ this.pesquisar }
         >
           Pesquisar
 
         </button>
+        <div>
+          {artistaInfo.length === 0 ? (<p>Nenhum álbum encontrado</p>) : (
+            artistaInfo.map((e, i) => (<>
+              <Link to={ `/album/${e.collectionId}` } data-testid={ `link-to-album-${e.collectionId}` }><img src={ e.artworkUrl100 } key={ i } /></Link>
+              {' '}
+              <p key={ i }>
+                Album 0
+                {i + 1}
+              </p>
+              {' '}
+              <p key={ i }>{e.artistName}</p>
+            </>))
+          )}
+
+        </div>
 
       </div>
     );
