6ad13aa19e33d8d549823ebd1b4a166c5a16b59e
Leonardo Elias Souza de Oliverira Meireles <leonardoelias802@gmail.com>
2022-10-05 12:54:37 -0300

    arrumando lint

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 class MusicCard extends React.Component {
-    render() {
-        const {nomeMusica, prevMusica} = this.props
-        return(
-        <>
-         <p>{nomeMusica}</p>
-         <audio data-testid="audio-component" src={prevMusica} controls>
-  <track kind="captions" />
-  O seu navegador não suporta o elemento{" "} <code>audio</code>.
-</audio>
-        </>
-        )
-    }
-}
+  render() {
+    const { nomeMusica, prevMusica } = this.props;
 
-export default MusicCard
+    return (
+      <>
+
+        <p>{nomeMusica}</p>
+        <audio data-testid="audio-component" src={ prevMusica } controls>
+          <track kind="captions" />
+          O seu navegador não suporta o elemento
+          {' '}
+          {' '}
+          <code>audio</code>
+          .
+        </audio>
+      </>
+    );
+  }
+}
+MusicCard.propTypes = {
+  nomeMusica: PropTypes.string.isRequired,
+  prevMusica: PropTypes.string.isRequired,
+};
+
+export default MusicCard;
--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,49 +1,60 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import getMusics from '../services/musicsAPI';
 import MusicCard from '../components/MusicCard';
 
 class Album extends React.Component {
   constructor() {
-    super()
+    super();
     this.state = {
       musicas: [],
-      parar:false,
-    }
+      parar: false,
+    };
   }
 
   buscandoMusicas = async (id) => {
-     const musicas = await getMusics(id)
-     this.setState({
-      musicas: musicas,
+    const musicas = await getMusics(id);
+    this.setState({
+      musicas,
       parar: true,
-     })
-  }
+    });
+  };
+
   render() {
-    let musicaFiltradas = []
-    const {id} = this.props.match.params;
-    const {musicas, parar} = this.state;
+    const { match } = this.props;
+    const { params } = match;
+    const { id } = params;
+    const { musicas, parar } = this.state;
     console.log(musicas);
-    if(parar === false) {
-      this.buscandoMusicas(id)
+    if (parar === false) {
+      this.buscandoMusicas(id);
     }
-      
+
     return (
       <div data-testid="page-album">
         <Header />
         <h1>album</h1>
         {parar ? (
-        <>
-        <p data-testid="artist-name">{musicas[0].artistName}</p>
-        <p data-testid="album-name">{musicas[0].collectionName}</p>
-         {musicas.filter((e) => e.kind === 'song').map((e,i) => (
-           <MusicCard key={i} nomeMusica={e.trackName} prevMusica={e.previewUrl}/>          
-         ))}
-        </>
-      ) : (<p>carregando</p>)}
+          <>
+            <p data-testid="artist-name">{musicas[0].artistName}</p>
+            <p data-testid="album-name">{musicas[0].collectionName}</p>
+            {musicas.filter((e) => e.kind === 'song').map((e, i) => (
+              <MusicCard
+                key={ i }
+                nomeMusica={ e.trackName }
+                prevMusica={ e.previewUrl }
+              />
+            ))}
+          </>
+        ) : (<p>carregando</p>)}
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.objectOf.isRequired,
+};
 
 export default Album;
